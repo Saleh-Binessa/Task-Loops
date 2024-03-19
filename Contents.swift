ba5d0b84a9ad694de8ae7afe9237096001ef0843
Saleh <saleh.binessa@gmail.com>
2024-03-19 23:31:05 +0300

    first commit

---
+++ Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+// Task 1: Iterating Over an Array
+let favoriteFruits = ["Apple", "Banana", "Cherry"]
+for fruits in favoriteFruits {
+    print(fruits)
+}
+
+// Task 2: Nested Loops with Strings
+for i in 1...5 {
+    for j in 1...5 {
+        let result = i * j
+        print("\(i) * \(j) = \(result)")
+    }
+}
